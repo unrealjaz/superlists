d2bf9c736d0a52782469a7814a63ef074e39b666
J. Armendariz <jacinto.armendariz@outlook.com>
2016-11-16 15:03:15 -0700

    new URL + view for adding to existing lists. FT passes :)

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,7 +2,14 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-)
+                       url(r'^$', 'lists.views.home_page', name='home'),
+
+                       url(r'^lists/(\d+)/$',
+                           'lists.views.view_list', name='view_list'),
+
+                       url(r'^lists/(\d+)/add_item$',
+                           'lists.views.add_item', name='add_item'),
+
+                       url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+
+                       )
